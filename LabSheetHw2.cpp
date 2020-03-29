f3ae1d34f5732b1fd436b4c755fc8b37c4245fa6
Laila Sayani <laila_snow_redrose@yahoo.com>
2020-03-29 16:37:54 +0300

    Update LabSheetHw2.cpp

--- LabSheetHw2.cpp
+++ LabSheetHw2.cpp
@@ -5,3 +5,20 @@
 #include<fstream>
 using namespace std;
 int main() {
+  
+  //Q4 task : multiplication table using while loop
+  
+	int i = 1;
+	while (i <= 10) {
+		int j = 1;
+		while (j <= 10) {
+			//cout << i<< "*"<< j <<"="<<i*j<< setw(10);
+			cout << i * j << setw(10);
+			j++;
+		}
+		cout << "\n";
+		i++;
+	}
+	return 0;
+
+}
