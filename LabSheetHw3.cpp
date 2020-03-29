ea0fa4fd1c1720e52f7ef73eda7c96d3bc222062
Laila Sayani <laila_snow_redrose@yahoo.com>
2020-03-29 17:48:54 +0300

    Update LabSheetHw3.cpp

--- LabSheetHw3.cpp
+++ LabSheetHw3.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include<iomanip>
+#include<string>
+#include<cmath>
+#include<fstream>
+using namespace std;
+int main() {
+  
+  //Q2 calculate the sum of the following series using nested loop
+  
+  
+  int res = 1;
+	int pow = 1;
+	int sum = 1;
+	int index = 1;
+	while (index <= n) {
+		while (pow <= index) {
+			res *= x;
+			sum += res;
+			pow++;
+		}
+		
+		index++;
+	}
+	cout << sum;
+  return 0;
 
+}
